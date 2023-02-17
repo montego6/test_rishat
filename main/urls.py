9f800f5f47195635ce90b63fef2d6e823ad3d7c5
Kirill Kirsanov <montego@yandex.ru>
2023-02-17 18:23:22 +0300

    Added item retrieve page and html template

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('buy/<int:id>/', views.item_retrieve, name='item-retrieve')
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
+def item_retrieve(request, id):
+    item = Item.objects.get(id=id)
+    return render(request, 'item.html', {'item': item})
