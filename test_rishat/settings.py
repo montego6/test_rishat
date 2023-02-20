becacbf4caa268003d0e7936071bf4a42ab01089
Kirill Kirsanov <montego@yandex.ru>
2023-02-20 18:23:53 +0300

    Allowed hosts added

--- test_rishat/settings.py
+++ test_rishat/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://testrishat-production.up.railway.app', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://testrishat-production.up.railway.app']
 
 
 # Application definition
